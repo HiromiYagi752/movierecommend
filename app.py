c83231437ea5dd418c3e0b0d671839958787964a
Guest1 <Guest1@MasahironoMacBook-Pro.local>
2024-02-10 13:53:03 +0900

    コミットメッセージ

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ selected_movie_title = st.selectbox(
 #     st.warning("選択された映画IDがありません。")
 
 # FastAPIサーバーのエンドポイント
-api_url = "https://movie-app-s5ff.onrender.com/get_similar_movies"    
+api_url = "https://_ _ _ _ _ .onrender.com/get_similar_movies"    
 
 # 選択された映画のIDから類似映画を取得
 response = requests.post(api_url, json={"movie_id": selected_movie_ids[0]})
